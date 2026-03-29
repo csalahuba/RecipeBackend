c096e3a41774f4f684e26667b179f839d7a84704
csalahuba <csala.huba@student.ms.sapientia.ro>
2026-03-30 01:08:58 +0300

    db

--- Program.cs
+++ Program.cs
@@ -5,7 +5,14 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<RecipeContext>(options => options.UseInMemoryDatabase("RecipeList"));
+var connectionString = builder.Configuration["RecipeURL"]
+    ?? builder.Configuration["AZURE_POSTGRESQL_CONNECTIONSTRING"]
+    ?? builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException(
+        "Az adatbazis kapcsolat hianyzik. Allitsd be a RecipeURL vagy DatabaseURL env valtozot, vagy a ConnectionStrings:DefaultConnection erteket.");
+
+builder.Services.AddDbContext<RecipeContext>(options =>
+    options.UseNpgsql(connectionString));
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -17,6 +24,12 @@ builder.Services.AddScoped<IRecipeRepository, RecipeRepository>();
 builder.Services.AddScoped<IRecipeService, RecipeService>();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<RecipeContext>();
+    db.Database.Migrate();
+}
 
 app.UseSwagger();
 app.UseSwaggerUI();
--- obj/Debug/net8.0/ReceptAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/ReceptAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReceptAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29328526752c4aa90d15d92f5edab2dfdcfd242d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe36e35d67d66af925cf9ad913d4984375b67155")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReceptAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReceptAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
